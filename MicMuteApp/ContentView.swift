25edaee1bdaf11552e30c08133d9e2205d3c50fa
dariuszpalt <paltdariusz@gmail.com>
2024-12-02 14:31:57 +0100

    Initial Commit

---
+++ MicMuteApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MicMuteApp
+//
+//  Created by Dariusz Palt on 02/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MicMuteApp/MicMuteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicMuteAppApp.swift
+//  MicMuteApp
+//
+//  Created by Dariusz Palt on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MicMuteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
